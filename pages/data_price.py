f0059011392aee607adece7452ddb7141ef6aad3
wildan14ar <wildan1423ar@gmail.com>
2023-12-19 15:07:23 +0700

    commit

--- pages/data_price.py
+++ pages/data_price.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 
 sheet_id = "11RwSeez-R99Zx9iek69GRjsiDV2jMLDPu4CmyexoXMM"
 sheet_name = "price"
