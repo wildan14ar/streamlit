cc423738ada235e1f7d82b397d7b1b999295db97
wildan14ar <wildan1423ar@gmail.com>
2023-12-23 09:41:35 +0700

    commit

--- pages/data_price.py
+++ pages/data_price.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 sheet_id = "11RwSeez-R99Zx9iek69GRjsiDV2jMLDPu4CmyexoXMM"
 sheet_name = "price"
@@ -13,7 +14,13 @@ gk=pd.DataFrame({'pil':["Preprosesing", "Analyst", "Modelling", "Prediksi"]})
 cfd=st.sidebar.selectbox('Please select:', gk['pil'])
 if cfd == "Preprosesing":
     st.subheader("Preprosessing data:")
+    code = '''
+df.head() # Menampilkan 5 data pertarma
+df.info()
+'''
+    st.code(code, language='python')
     st.dataframe(df.head())
+    st.write(df.info())
     code = '''df.dtypes'''
     st.code(code, language='python')
     st.text(df.dtypes)
@@ -21,15 +28,32 @@ if cfd == "Preprosesing":
     st.code(code, language='python')
     st.text(df.isnull().sum())
     df = df.dropna()
-    code = '''df = df.dropna() 
+    code = '''
+df = df.dropna() 
 df.isnull().sum()
-df.head()'''
+df.head() # Menampilkan 5 data pertarma
+'''
     st.code(code, language='python')
     st.text(df.isnull().sum())
     st.dataframe(df.head())
 
 if cfd == "Analyst":
     st.subheader("Analyst visual data")
+    df = df.dropna()
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Total rumah",  df['Observation'].count())
+    col2.metric("Builtup", round(df['Builtup'].mean()))
+    col3.metric("Carpet", round(df['Carpet'].mean()))
+    col4.metric("House Price", round(df['House_Price'].mean()))
+    st.dataframe(df.head())
+    fig, ax = plt.subplots()
+    ax.bar(df['City_Category'], df['House_Price'])
+    ax.set_xlabel('City Category')
+    ax.set_ylabel('House Price')
+    st.pyplot(fig)
+
+    
+    
 
 
 if cfd == "Modelling":
